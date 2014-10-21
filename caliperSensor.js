dfc69f188d15bc0dcfb49514408ca0ff317fee6d
Prashant Nayak <prashant@intellifylearning.com>
2014-10-21 15:56:03 -0400

    continue to flesh out scaffolding for sensor

--- caliperSensor.js
+++ caliperSensor.js
@@ -2,10 +2,21 @@
 // if it doesn't exist
 var CaliperSensor = window.CaliperSensor || {};
 
+CaliperSensor.send = function(caliperEvent) {
+	//
+};
+
+CaliperSensor.describe = function(caliperEntity) {
+	//
+};
+
+
 // Stick on the modules that need to be exported.
 // You only need to require the top-level modules, browserify
 // will walk the dependency graph and load everything correctly
 CaliperSensor.RoadRunner = require('./actions');
+CaliperSensor.RoadRunner = require('./caliperEntity');
+CaliperSensor.RoadRunner = require('./caliperEvent');
 
 // Replace/Create the global namespace
 window.CaliperSensor = CaliperSensor;
