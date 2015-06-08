92deadddea358467f3fab4228eaf5b9e39af041f
Anthony Whyte <anthwhyte@gmail.com>
2015-06-08 18:09:28 -0400

    Remove Agent @type URI; tweek Response @type URIs; move MediaLocation @URI to digitalResourceType.  Resolves #43

--- src/entities/response/responseType.js
+++ src/entities/response/responseType.js
@@ -17,11 +17,11 @@
  */
 
 var responseType = {
-    "FILLINBLANK": "http://purl.imsglobal.org/caliper/v1/Response/FillinBlank",
-    "MULTIPLECHOICE": "http://purl.imsglobal.org/caliper/v1/Response/MultipleChoice",
-    "MULTIPLERESPONSE": "http://purl.imsglobal.org/caliper/v1/Response/MultipleResponse",
-    "SELECTTEXT": "http://purl.imsglobal.org/caliper/v1/Response/SelectText",
-    "TRUEFALSE": "http://purl.imsglobal.org/caliper/v1/Response/TrueFalse"
+    "FILLINBLANK": "http://purl.imsglobal.org/caliper/v1/FillinBlankResponse",
+    "MULTIPLECHOICE": "http://purl.imsglobal.org/caliper/v1/MultipleChoiceResponse",
+    "MULTIPLERESPONSE": "http://purl.imsglobal.org/caliper/v1/MultipleResponseResponse",
+    "SELECTTEXT": "http://purl.imsglobal.org/caliper/v1/SelectTextResponse",
+    "TRUEFALSE": "http://purl.imsglobal.org/caliper/v1/TrueFalseResponse"
 };
 
 module.exports = responseType;
