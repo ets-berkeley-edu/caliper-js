a017614b968d1937f32f7b36eede41a5874dce4a
Anthony Whyte <anthwhyte@gmail.com>
2016-07-13 14:56:07 -0400

    Clean up requires section

--- src/client.js
+++ src/client.js
@@ -16,7 +16,6 @@
  * with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-var _ = require('lodash');
 var requestor = require('./request/httpRequestor');
 var logger = require('./logger');
 
--- src/events/eventFactory.js
+++ src/events/eventFactory.js
@@ -17,7 +17,6 @@
  */
 
 var _ = require('lodash');
-var constants = require('../constants');
 var Event = require('./event');
 var validator = require('./eventValidator');
 
