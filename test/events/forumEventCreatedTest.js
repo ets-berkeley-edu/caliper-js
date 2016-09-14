caded6aa51db3420dda848af38eb6dede60a7fbd
Anthony Whyte <anthwhyte@gmail.com>
2016-09-14 15:14:34 -0400

    Update ForumEventCreatedTest.js

--- test/events/forumEventCreatedTest.js
+++ test/events/forumEventCreatedTest.js
@@ -16,11 +16,9 @@
  * with this program. If not, see http://www.gnu.org/licenses/.
  */
 
+var _ = require('lodash');
 var moment = require('moment');
 var test = require('tape');
-var _ = require('lodash');
-var util = require('util');
-var jsonCompare = require('../testUtils');
 
 // Event
 var eventFactory = require('../../src/events/eventFactory');
@@ -41,54 +39,60 @@ var ForumActions = require('../../src/actions/forumActions');
 var Role = require('../../src/entities/lis/role');
 var Status = require('../../src/entities/lis/status');
 
+var jsonCompare = require('../testUtils');
+
 test('Create a ForumEvent (created) and validate properties', function (t) {
 
   // Plan for N assertions
   t.plan(1);
 
-  const BASE_COURSE_IRI = "https://example.edu/semesters/201601/courses/25";
-  const BASE_LMS_IRI = "https://example.com/lms";
+  const BASE_IRI = "https://example.edu";
+  const BASE_SECTION_IRI = "https://example.edu/terms/201601/courses/7/sections/1";
 
-  // The Actor for the Caliper Event
-  var actorId = "https://example.edu/user/223344";
-  var actor = entityFactory().create(Person, actorId);
+  // The Actor
+  var actor = entityFactory().create(Person, BASE_IRI.concat("/users/554433"));
 
-  // The Action for the Caliper Event
-  var action = ForumActions.CREATED;
+  // The Action
+  var action = ForumActions.SUBSCRIBED;
 
-  // Forum object
-  var obj = entityFactory().create(Forum, BASE_COURSE_IRI.concat("/forums/1"), {
-    name: "Caliper Forum",
-    dateCreated: "2016-09-15T10:15:00.000Z"
+  // Course Section (Group context)
+  var group = entityFactory().create(CourseSection, BASE_SECTION_IRI, {
+    courseNumber: "CPS 435-01",
+    academicSession: "Fall 2016"
   });
+
+  // The Object of the interaction
+  var obj = entityFactory().create(Forum, BASE_SECTION_IRI.concat("/forums/1"), {
+    name: "Caliper Forum",
+    isPartOf: _.omit(group, ["courseNumber", "academicSession"]),
+    dateCreated: "2016-09-14T11:00:00.000Z"
+  });
+
+  // Event time
+  var eventTime = moment.utc("2016-11-15T10:16:00.000Z");
 
   // edApp context
-  var edApp = entityFactory().create(SoftwareApplication, BASE_LMS_IRI.concat("/forums"), { version: "v2" });
+  var edApp = entityFactory().create(SoftwareApplication, BASE_IRI.concat("/forums"), { version: "v2" });
 
-  // Group context
-  var group = entityFactory().create(CourseSection, BASE_COURSE_IRI.concat("/sections/1"), {
-    courseNumber: "POL101-01",
-    academicSession: "Fall-2016"
+  // The Actor's Membership
+  var membership = entityFactory().create(Membership, BASE_SECTION_IRI.concat("/rosters/1"), {
+    member: actor,
+    organization: _.omit(group, ["courseNumber", "academicSession"]),
+    roles: [Role.LEARNER],
+    status: Status.ACTIVE,
+    dateCreated: moment.utc("2016-08-01T06:00:00.000Z")
   });
 
-  // Actor's membership context
-  var membership = entityFactory().create(Membership, BASE_COURSE_IRI.concat("/rosters/1"), {
-    member: actor['@id'],
-    organization: group['@id'],
-    roles: [Role.INSTRUCTOR],
-    status: Status.ACTIVE
-  });
-
-  // Local Session
-  var sessionId = BASE_LMS_IRI.concat("/sessions/65b00087bc3303f60b4045d8bf54107ae58e8636");
-  var session = entityFactory().create(Session, sessionId, { startedAtTime: moment.utc("2016-09-15T10:13:00.000Z") });
+  // Session
+  var session = entityFactory().create(Session, BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259"), {
+    startedAtTime: moment.utc("2016-11-15T10:00:00.000Z") });
 
   // Assert that key attributes are the same
   var event = eventFactory().create(ForumEvent, {
     actor: actor,
     action: action,
     object: obj,
-    eventTime: moment.utc("2016-09-15T10:15:00.000Z"),
+    eventTime: eventTime,
     edApp: edApp,
     group: group,
     membership: membership,
@@ -96,5 +100,5 @@ test('Create a ForumEvent (created) and validate properties', function (t) {
   });
 
   // Assert that the JSON produced is the same
-  jsonCompare('caliperEventForumCreated', event, t);
+  jsonCompare('caliperEventForumSubscribed', event, t);
 });
