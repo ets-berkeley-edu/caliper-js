761de05f00b082188a3a7f74b1cd3b4015c1e3b3
Anthony Whyte <anthwhyte@gmail.com>
2016-09-14 08:34:56 -0400

    Update sessionEventLoggedInTest

--- test/events/sessionEventLoggedInTest.js
+++ test/events/sessionEventLoggedInTest.js
@@ -24,17 +24,9 @@ var SessionEvent = require('../../src/events/sessionEvent');
 var SessionActions = require('../../src/actions/sessionActions');
 
 var entityFactory = require('../../src/entities/entityFactory');
-var CourseOffering = require('../../src/entities/lis/courseOffering');
-var CourseSection = require('../../src/entities/lis/courseSection');
-var EpubVolume = require('../../src/entities/resource/ePubVolume');
-var Frame = require('../../src/entities/resource/frame');
-var Group = require('../../src/entities/lis/group');
-var Membership = require('../../src/entities/lis/membership');
 var Person = require('../../src/entities/agent/person');
-var Role = require('../../src/entities/lis/role');
 var Session = require('../../src/entities/session/session');
 var SoftwareApplication = require('../../src/entities/agent/SoftwareApplication');
-var Status = require('../../src/entities/lis/status');
 
 var jsonCompare = require('../testUtils');
 
@@ -43,115 +35,40 @@ test('Create a SessionEvent (loggedIn) and validate properties', function (t) {
   // Plan for N assertions
   t.plan(1);
 
-  const BASE_COURSE_IRI = "https://example.edu/politicalScience/2015/american-revolution-101";
-  const BASE_VIEWER_IRI = "https://example.com/viewer";
-  const BASE_EPUB_IRI = "https://example.com/viewer/book/34843";
+  const BASE_IRI = "https://example.edu";
 
   // The Actor for the Caliper Event (as well as the edApp)
-  var actorId = "https://example.edu/user/554433";
-  var actor = entityFactory().create(Person, actorId, {
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
+  var actorId = BASE_IRI.concat("/users/554433");
+  var actor = entityFactory().create(Person, actorId);
 
   // The Action for the Caliper Event
   var action = SessionActions.LOGGED_IN;
 
   // The Object being interacted with by the Actor
-  var obj = entityFactory().create(SoftwareApplication, BASE_VIEWER_IRI, {
-    name: "ePub",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    version: "1.2.3"
-  });
+  var obj = entityFactory().create(SoftwareApplication, BASE_IRI, { version: "v2" });
 
-  // Generated session
-  var generated = entityFactory().create(Session, BASE_VIEWER_IRI.concat("/session-123456789"), {
-    name: "session-123456789",
+  // Event time
+  var eventTime = moment.utc("2016-11-15T10:15:00.000Z");
+
+  // Session
+  var sessionId = BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259");
+  var session = entityFactory().create(Session, sessionId, {
     actor: actor,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    startedAtTime: moment.utc("2015-09-15T10:15:00.000Z")
+    dateCreated: moment.utc("2016-11-15T10:00:00.000Z"),
+    startedAtTime: moment.utc("2016-11-15T10:00:00.000Z")
   });
 
-  // ePub parent
-  var ePub = entityFactory().create(EpubVolume, BASE_EPUB_IRI.concat("#epubcfi(/4/3)"), {
-    name: "The Glorious Cause: The American Revolution, 1763-1789 (Oxford History of the United States)",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    version: "2nd ed."
-  });
-
-  // The target object (frame) within the Event Object
-  var target = entityFactory().create(Frame, BASE_EPUB_IRI.concat("#epubcfi(/4/3/1)"), {
-    name: "Key Figures: George Washington",
-    isPartOf: ePub,
-    index: 1,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    version: ePub.version
-  });
-
-  // The edApp
-  var edApp = entityFactory().create(SoftwareApplication, BASE_VIEWER_IRI, {
-    name: "ePub",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    version: "1.2.3"
-  });
-
-  // LIS Course Offering
-  var course = entityFactory().create(CourseOffering, BASE_COURSE_IRI, {
-    name: "Political Science 101: The American Revolution",
-    courseNumber: "POL101",
-    academicSession: "Fall-2015",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
-
-  // LIS Course Section
-  var sectionId = BASE_COURSE_IRI.concat("/section/001");
-  var section = entityFactory().create(CourseSection, sectionId, {
-    name: "American Revolution 101",
-    courseNumber: "POL101",
-    academicSession: "Fall-2015",
-    subOrganizationOf: course,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
-
-  // LIS Group
-  var groupId = sectionId.concat("/group/001");
-  var group = entityFactory().create(Group, groupId, {
-    name: "Discussion Group 001",
-    subOrganizationOf: section,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z")
-  });
-
-  // The Actor's Membership
-  var membershipId = BASE_COURSE_IRI.concat("/roster/554433");
-  var membership = entityFactory().create(Membership, membershipId, {
-    name: "American Revolution 101",
-    description: "Roster entry",
-    member: actor['@id'],
-    organization: section['@id'],
-    roles: [Role.LEARNER],
-    status: Status.ACTIVE,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z")
-  });
+  // Event Id GUID
+  var eventId = "341db3d9-71cc-4081-9423-cbed73cb0179";
 
   // Assert that key attributes are the same
   var event = eventFactory().create(SessionEvent, {
-    id: "15128c13-ca75-4952-8cce-72a513ec337d",
+    id: eventId,
     actor: actor,
     action: action,
     object: obj,
-    eventTime: moment.utc("2015-09-15T10:15:00.000Z"),
-    generated: generated,
-    target: target,
-    edApp: edApp,
-    group: group,
-    membership: membership
+    eventTime: eventTime,
+    session: session
   });
 
   // Assert that the JSON produced is the same
