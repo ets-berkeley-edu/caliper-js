ab12fd3a71d4b19ad4fbbe39fc2ad7b4dd6f5fca
Anthony Whyte <anthwhyte@gmail.com>
2016-09-16 15:18:30 -0400

    Update assessmentItemEventCompletedTest

--- test/events/assessmentItemEventCompletedTest.js
+++ test/events/assessmentItemEventCompletedTest.js
@@ -16,6 +16,7 @@
  * with this program. If not, see http://www.gnu.org/licenses/.
  */
 
+var _ = require('lodash');
 var moment = require('moment');
 var test = require('tape');
 
@@ -27,13 +28,12 @@ var entityFactory = require('../../src/entities/entityFactory');
 var Assessment = require('../../src/entities/resource/assessment');
 var AssessmentItem = require('../../src/entities/resource/assessmentItem');
 var Attempt = require('../../src/entities/assign/attempt');
-var CourseOffering = require('../../src/entities/lis/courseOffering');
 var CourseSection = require('../../src/entities/lis/courseSection');
 var FillinBlankResponse = require('../../src/entities/response/fillinBlankResponse');
-var Group = require('../../src/entities/lis/group');
 var Membership = require('../../src/entities/lis/membership');
 var Person = require('../../src/entities/agent/person');
 var Role = require('../../src/entities/lis/role');
+var Session = require('../../src/entities/session/session');
 var SoftwareApplication = require('../../src/entities/agent/SoftwareApplication');
 var Status = require('../../src/entities/lis/status');
 
@@ -44,114 +44,71 @@ test('Create an AssessmentItemEvent (completed) and validate properties', functi
   // Plan for N assertions
   t.plan(1);
 
-  const BASE_COURSE_IRI = "https://example.edu/politicalScience/2015/american-revolution-101";
+  const BASE_IRI = "https://example.edu";
+  const BASE_SECTION_IRI = "https://example.edu/terms/201601/courses/7/sections/1";
+  const BASE_ASSESS_IRI = "https://example.edu/terms/201601/courses/7/sections/1/assess/1";
+  const BASE_ITEM_IRI = "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3";
 
-  // The Actor for the Caliper Event
-  var actorId = "https://example.edu/user/554433";
-  var actor = entityFactory().create(Person, actorId, {
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
+  // Actor
+  var actor = entityFactory().create(Person, BASE_IRI.concat("/users/554433"));
 
   // The Action for the Caliper Event
   var action = AssessmentItemActions.COMPLETED;
 
-  // The Object being interacted with by the Actor (Assessment)
-  var parentId = BASE_COURSE_IRI.concat("/assessment/001");
-  var parent = entityFactory().create(Assessment, parentId, {
-    name: "American Revolution - Key Figures Assessment",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z"),
-    datePublished: moment.utc("2015-08-15T09:30:00.000Z"),
-    version: "1.0",
-    dateToActivate: moment.utc("2015-08-16T05:00:00.000Z"),
-    dateToShow: moment.utc("2015-08-16T05:00:00.000Z"),
-    dateToStartOn: moment.utc("2015-08-16T05:00:00.000Z"),
-    dateToSubmit: moment.utc("2015-09-28T11:59:59.000Z"),
-    maxAttempts: 2,
-    maxSubmits: 2,
-    maxScore: 3.0
+  // Parent assessment
+  var parent = entityFactory().create(Assessment, BASE_ASSESS_IRI);
+  var parentAttempt = entityFactory().create(Attempt, BASE_ASSESS_IRI.concat("/users/554433/attempts/1"));
+
+  // Item
+  var item = entityFactory().create(AssessmentItem, BASE_ASSESS_IRI.concat("/items/3"), {
+    name: "Assessment Item 3",
+    isPartOf: parent
   });
 
-  // The Object being interacted with by the Actor (AssessmentItem)
-  var objId = parentId.concat("/item/001");
-  var obj = entityFactory().create(AssessmentItem, objId, {
-    name: "Assessment Item 1",
-    isPartOf: parent,
-    maxAttempts: 2,
-    maxSubmits: 2,
-    maxScore: 1.0,
-    isTimeDependent: false,
-    version: "1.0"
-  });
-
-  // The learner's attempt
-  var attemptId = objId.concat("/attempt/789");
-  var attempt = entityFactory().create(Attempt, attemptId, {
-    actor: actor['@id'],
-    assignable: parent['@id'],
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    startedAtTime: moment.utc("2015-09-15T10:15:00.000Z"),
+  // The Object of the interaction
+  var obj = entityFactory().create(Attempt, BASE_ITEM_IRI.concat("/users/554433/attempts/1"), {
+    actor: actor,
+    assignable: item,
+    isPartOf: parentAttempt,
+    dateCreated: moment.utc("2016-11-15T10:15:02.000Z"),
+    startedAtTime: moment.utc("2016-11-15T10:15:02.000Z"),
+    endedAtTime: moment.utc("2016-11-15T10:15:12.000Z"),
     count: 1
   });
 
+  // Event time
+  var eventTime = moment.utc("2016-11-15T10:15:12.000Z");
+
   // The generated response
-  var generatedId = objId.concat("/response/001");
-  var generated = entityFactory().create(FillinBlankResponse, generatedId, {
-    actor: actor['@id'],
-    assignable: parent['@id'],
-    attempt: attempt,
-    values: ["2 July 1776"],
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    startedAtTime: moment.utc("2015-09-15T10:15:00.000Z")
+  var generated = entityFactory().create(FillinBlankResponse, BASE_ITEM_IRI.concat("/users/554433/responses/1"), {
+    attempt: _.omit(obj, [ "actor", "assignable", "isPartOf", "dateCreated", "startedAtTime", "endedAtTime", "count" ]),
+    values: [ "subject", "object", "predicate" ],
+    dateCreated: moment.utc("2016-11-15T10:15:12.000Z"),
+    startedAtTime: moment.utc("2016-11-15T10:15:02.000Z"),
+    endedAtTime: moment.utc("2016-11-15T10:15:12.000Z")
   });
 
   // The edApp
-  var edAppId = "https://example.com/super-assessment-tool";
-  var edApp = entityFactory().create(SoftwareApplication, edAppId, {
-    name: "Super Assessment Tool",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    version: "v2"
+  var edApp = entityFactory().create(SoftwareApplication, BASE_IRI, { version: "v2" });
+
+  // Group
+  var group = entityFactory().create(CourseSection, BASE_SECTION_IRI, {
+    courseNumber: "CPS 435-01",
+    academicSession: "Fall 2016"
   });
 
-  // LIS Course Offering
-  var course = entityFactory().create(CourseOffering, BASE_COURSE_IRI, {
-    name: "Political Science 101: The American Revolution",
-    courseNumber: "POL101",
-    academicSession: "Fall-2015",
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
-
-  // LIS Course Section
-  var sectionId = BASE_COURSE_IRI.concat("/section/001");
-  var section = entityFactory().create(CourseSection, sectionId, {
-    name: "American Revolution 101",
-    courseNumber: "POL101",
-    academicSession: "Fall-2015",
-    subOrganizationOf: course,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z"),
-    dateModified: moment.utc("2015-09-02T11:30:00.000Z")
-  });
-
-  // LIS Group
-  var groupId = sectionId.concat("/group/001");
-  var group = entityFactory().create(Group, groupId, {
-    name: "Discussion Group 001",
-    subOrganizationOf: section,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z")
-  });
-
-  // The Actor's Membership
-  var membershipId = BASE_COURSE_IRI.concat("/roster/554433");
-  var membership = entityFactory().create(Membership, membershipId, {
-    name: "American Revolution 101",
-    description: "Roster entry",
-    member: actor['@id'],
-    organization: section['@id'],
+  // Membership
+  var membership = entityFactory().create(Membership, BASE_SECTION_IRI.concat("/rosters/1"), {
+    member: actor,
+    organization: _.omit(group, ["courseNumber", "academicSession"]),
     roles: [Role.LEARNER],
     status: Status.ACTIVE,
-    dateCreated: moment.utc("2015-08-01T06:00:00.000Z")
+    dateCreated: moment.utc("2016-08-01T06:00:00.000Z")
+  });
+
+  // Session
+  var session = entityFactory().create(Session, BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259"), {
+    startedAtTime: moment.utc("2016-11-15T10:00:00.000Z")
   });
 
   // Assert that key attributes are the same
@@ -159,13 +116,97 @@ test('Create an AssessmentItemEvent (completed) and validate properties', functi
     actor: actor,
     action: action,
     object: obj,
-    eventTime: moment.utc("2015-09-15T10:15:00.000Z"),
+    eventTime: eventTime,
     generated: generated,
     edApp: edApp,
     group: group,
-    membership: membership
+    membership: membership,
+    session: session
+
   });
 
   // Assert that the JSON produced is the same
   jsonCompare('caliperEventAssessmentItemCompleted', event, t);
 });
+
+/**
+ {
+  "@context": "http://purl.imsglobal.org/ctx/caliper/v1/Context",
+  "@type": "http://purl.imsglobal.org/caliper/v1/AssessmentItemEvent",
+  "actor": {
+    "@id": "https://example.edu/users/554433",
+    "@type": "http://purl.imsglobal.org/caliper/v1/Person"
+  },
+  "action": "http://purl.imsglobal.org/vocab/caliper/v1/action#Completed",
+  "object": {
+    "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3/users/554433/attempts/1",
+    "@type": "http://purl.imsglobal.org/caliper/v1/Attempt",
+    "actor": {
+      "@id": "https://example.edu/users/554433",
+      "@type": "http://purl.imsglobal.org/caliper/v1/Person"
+    },
+    "assignable": {
+      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3",
+      "@type": "http: //purl.imsglobal.org/caliper/v1/AssessmentItem",
+      "name": "Assessment Item 3",
+      "isPartOf": {
+        "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1",
+        "@type": "http://purl.imsglobal.org/caliper/v1/Assessment"
+      }
+    },
+    "isPartOf": {
+      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/users/554433/attempts/1",
+      "@type": "http://purl.imsglobal.org/caliper/v1/Attempt"
+    },
+    "count": 1,
+    "dateCreated": "2016-11-15T10:15:02.000Z",
+    "startedAtTime": "2016-11-15T10:15:02.000Z",
+    "endedAtTime": "2016-11-15T10:15:12.000Z"
+  },
+  "generated": {
+    "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3/users/554433/responses/1",
+    "@type": "http://purl.imsglobal.org/caliper/v1/FillinBlankResponse",
+    "attempt": {
+      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3/users/554433/attempts/1",
+      "@type": "http://purl.imsglobal.org/caliper/v1/Attempt"
+    },
+    "dateCreated": "2016-11-15T10:15:12.000Z",
+    "startedAtTime": "2016-11-15T10:15:02.000Z",
+    "endedAtTime": "2016-11-15T10:15:12.000Z",
+    "values": [ "subject", "object", "predicate" ]
+  },
+  "eventTime": "2016-11-15T10:15:12.000Z",
+  "edApp": {
+    "@id": "https://example.edu/",
+    "@type": "http://purl.imsglobal.org/caliper/v1/SoftwareApplication",
+    "version": "v2"
+  },
+  "group": {
+    "@id": "https://example.edu/terms/201601/courses/7/sections/1",
+    "@type": "http://purl.imsglobal.org/caliper/v1/CourseSection",
+    "courseNumber": "CPS 435-01",
+    "academicSession": "Fall 2016"
+  },
+  "membership": {
+    "@id": "https://example.edu/terms/201601/courses/7/sections/1/rosters/1",
+    "@type": "http://purl.imsglobal.org/caliper/v1/Membership",
+    "member": {
+      "@id": "https://example.edu/users/554433",
+      "@type": "http://purl.imsglobal.org/caliper/v1/Person"
+    },
+    "organization": {
+      "@id": "https://example.edu/terms/201601/courses/7/sections/1",
+      "@type": "http://purl.imsglobal.org/caliper/v1/CourseSection"
+    },
+    "roles": [ "http://purl.imsglobal.org/vocab/lis/v2/membership#Learner" ],
+    "status": "http://purl.imsglobal.org/vocab/lis/v2/status#Active",
+    "dateCreated": "2016-08-01T06:00:00.000Z"
+  },
+  "session": {
+    "@id": "https://example.edu/sessions/1f6442a482de72ea6ad134943812bff564a76259",
+    "@type": "http://purl.imsglobal.org/caliper/v1/Session",
+    "startedAtTime": "2016-11-15T10:00:00.000Z"
+  }
+}
+
+ */
