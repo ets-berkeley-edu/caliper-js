5906592fbf7366c973855c20396353afa45d0579
Anthony Whyte <anthwhyte@gmail.com>
2016-09-18 09:55:58 -0400

    Add new assessmentItemEventSkippedTest

---
+++ test/events/assessmentItemEventSkippedTest.js
@@ -0,0 +1,114 @@
+/*
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+var _ = require('lodash');
+var moment = require('moment');
+var test = require('tape');
+
+// Event
+var eventFactory = require('../../src/events/eventFactory');
+var AssessmentItemEvent = require('../../src/events/assessmentItemEvent');
+var AssessmentItemActions = require('../../src/actions/assessmentItemActions');
+
+// Entity
+var entityFactory = require('../../src/entities/entityFactory');
+var Assessment = require('../../src/entities/resource/assessment');
+var AssessmentItem = require('../../src/entities/resource/assessmentItem');
+var CourseSection = require('../../src/entities/lis/courseSection');
+var Membership = require('../../src/entities/lis/membership');
+var Person = require('../../src/entities/agent/person');
+var Role = require('../../src/entities/lis/role');
+var Session = require('../../src/entities/session/session');
+var SoftwareApplication = require('../../src/entities/agent/softwareApplication');
+var Status = require('../../src/entities/lis/status');
+
+var jsonCompare = require('../testUtils');
+
+test('Create an AssessmentItemEvent (skipped) and validate properties', function (t) {
+
+  // Plan for N assertions
+  t.plan(1);
+
+  const BASE_IRI = "https://example.edu";
+  const BASE_SECTION_IRI = "https://example.edu/terms/201601/courses/7/sections/1";
+  const BASE_ASSESS_IRI = "https://example.edu/terms/201601/courses/7/sections/1/assess/1";
+  const BASE_ITEM_IRI = "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3";
+
+  // Actor
+  var actor = entityFactory().create(Person, BASE_IRI.concat("/users/554433"));
+
+  // Action
+  var action = AssessmentItemActions.SKIPPED;
+
+  // Parent assessment
+  var parent = entityFactory().create(Assessment, BASE_ASSESS_IRI);
+
+  // Object of the interaction
+  var obj = entityFactory().create(AssessmentItem, BASE_ASSESS_IRI.concat("/items/2"), {
+    name: "Assessment Item 2",
+    isPartOf: parent,
+    dateToStartOn: moment.utc("2016-11-14T05:00:00.000Z"),
+    dateToSubmit: moment.utc("2016-11-18T11:59:59.000Z"),
+    maxAttempts: 2,
+    maxSubmits: 2,
+    maxScore: 1,
+    isTimeDependent: false,
+    version: "1.0"
+  });
+
+  // Event time
+  var eventTime = moment.utc("2016-11-15T10:14:30.000Z");
+
+  // The edApp
+  var edApp = entityFactory().create(SoftwareApplication, BASE_IRI, { version: "v2" });
+
+  // Group
+  var group = entityFactory().create(CourseSection, BASE_SECTION_IRI, {
+    courseNumber: "CPS 435-01",
+    academicSession: "Fall 2016"
+  });
+
+  // Membership
+  var membership = entityFactory().create(Membership, BASE_SECTION_IRI.concat("/rosters/1"), {
+    member: actor,
+    organization: _.omit(group, ["courseNumber", "academicSession"]),
+    roles: [Role.LEARNER],
+    status: Status.ACTIVE,
+    dateCreated: moment.utc("2016-08-01T06:00:00.000Z")
+  });
+
+  // Session
+  var session = entityFactory().create(Session, BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259"), {
+    startedAtTime: moment.utc("2016-11-15T10:00:00.000Z")
+  });
+
+  // Assert that key attributes are the same
+  var event = eventFactory().create(AssessmentItemEvent, {
+    actor: actor,
+    action: action,
+    object: obj,
+    eventTime: eventTime,
+    edApp: edApp,
+    group: group,
+    membership: membership,
+    session: session
+  });
+
+  // Assert that the JSON produced is the same
+  jsonCompare('caliperEventAssessmentItemSkipped', event, t);
+});
