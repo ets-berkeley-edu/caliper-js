5f628956b3f6f3df6806093c296b8e39eb3d4462
Anthony Whyte <anthwhyte@gmail.com>
2016-09-22 10:57:15 -0400

    Remove unnecessary require statements, commented out JSON

--- test/entities/assessmentTest.js
+++ test/entities/assessmentTest.js
@@ -20,22 +20,10 @@ var _ = require('lodash');
 var moment = require('moment');
 var test = require('tape');
 
-var eventFactory = require('../../src/events/eventFactory');
-var AssessmentEvent = require('../../src/events/assessmentEvent');
-var AssessmentActions = require('../../src/actions/assessmentActions');
-
 // Entity
 var entityFactory = require('../../src/entities/entityFactory');
 var Assessment = require('../../src/entities/resource/assessment');
 var AssessmentItem = require('../../src/entities/resource/assessmentItem');
-var Attempt = require('../../src/entities/assign/attempt');
-var CourseSection = require('../../src/entities/lis/courseSection');
-var Membership = require('../../src/entities/lis/membership');
-var Person = require('../../src/entities/agent/person');
-var Role = require('../../src/entities/lis/role');
-var Session = require('../../src/entities/session/session');
-var SoftwareApplication = require('../../src/entities/agent/softwareApplication');
-var Status = require('../../src/entities/lis/status');
 
 var jsonCompare = require('../testUtils');
 
@@ -66,45 +54,7 @@ test('Create an Assessment entity and validate properties', function (t) {
     maxScore: 15,
     version: "1.0"
   });
-
-
+  
   // Assert that the JSON produced is the same
   jsonCompare('caliperEntityAssessment', assess, t);
 });
-
-
-
-
-/**
- {
-  "@context": "http://purl.imsglobal.org/ctx/caliper/v1/Context",
-  "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1",
-  "@type": "http://purl.imsglobal.org/caliper/v1/Assessment",
-  "name": "Quiz One",
-  "items": [
-    {
-      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/1",
-      "@type": "http: //purl.imsglobal.org/caliper/v1/AssessmentItem"
-    },
-    {
-      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/2",
-      "@type": "http: //purl.imsglobal.org/caliper/v1/AssessmentItem"
-    },
-    {
-      "@id": "https://example.edu/terms/201601/courses/7/sections/1/assess/1/items/3",
-      "@type": "http: //purl.imsglobal.org/caliper/v1/AssessmentItem"
-    }
-  ],
-  "dateCreated": "2016-08-01T06:00:00.000Z",
-  "dateModified": "2016-09-02T11:30:00.000Z",
-  "datePublished": "2016-08-15T09:30:00.000Z",
-  "dateToActivate": "2016-08-16T05:00:00.000Z",
-  "dateToShow": "2016-08-16T05:00:00.000Z",
-  "dateToStartOn": "2016-08-16T05:00:00.000Z",
-  "dateToSubmit": "2016-09-28T11:59:59.000Z",
-  "maxAttempts": 2,
-  "maxScore": 15,
-  "maxSubmits": 2,
-  "version": "1.0"
-}
- */
