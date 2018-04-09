28f264be3bb49a28f12412e7a3d2511d6d877809
Anthony Whyte <anthwhyte@gmail.com>
2018-04-09 17:41:02 -0400

    Remove http-server references.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,21 +13,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    'http-server': {
-      'dev': {
-        // the server root directory
-        root: '.',
-        port: 8888,
-        host: "127.0.0.1",
-        cache: 0,
-        showDir: true,
-        autoIndex: true,
-        // server default file extension
-        ext: "html",
-        // run in parallel with other tasks
-        runInBackground: false
-      }
-    },
     tape: {
       options: {
         pretty: true,
@@ -52,9 +37,6 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "browserify" task.
   grunt.loadNpmTasks('grunt-browserify');
-
-  // Load plugin that provides http-server task
-  grunt.loadNpmTasks('grunt-http-server');
 
   // Load jsDoc3 plugin
   grunt.loadNpmTasks('grunt-jsdoc');
