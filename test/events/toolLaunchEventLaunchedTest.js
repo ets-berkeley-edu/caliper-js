32f5bf780fad63b888f6bf64888f0bb550c8d71a
Anthony Whyte <anthwhyte@gmail.com>
2018-04-20 15:18:32 -0400

    Update LtiSession messageParameters.

--- test/events/toolLaunchEventLaunchedTest.js
+++ test/events/toolLaunchEventLaunchedTest.js
@@ -28,6 +28,7 @@ var actions = require('../../src/actions/actions');
 
 var entityFactory = require('../../src/entities/entityFactory');
 var CourseSection = require('../../src/entities/agent/courseSection');
+var LtiSession = require('../../src/entities/session/ltiSession');
 var Membership = require('../../src/entities/agent/membership');
 var Person = require('../../src/entities/agent/person');
 var Role = require('../../src/entities/agent/role');
@@ -97,6 +98,34 @@ testUtils.readFile(path, function(err, fixture) {
       startedAtTime: moment.utc("2018-11-15T10:00:00.000Z")
     });
     
+    // LTI-related message parameters
+    var messageParameters = {
+      lti_message_type: "basic-lti-launch-request",
+      lti_version: "LTI-1p0",
+      context_id: "https://example.edu/terms/201801/courses/7/sections/1",
+      context_type: "urn:lti:context-type:ims/lis/CourseSection",
+      context_label: "CPS 435-01",
+      context_title: "CPS 435 Learning Analytics, Section 01",
+      resource_link_id: "6b37a950-42c9-4117-8f4f-03e6e5c88d24",
+      roles: [ "urn:lti:role:ims/lis/Learner" ],
+      tool_consumer_instance_guid: "SomeLMS.example.edu",
+      tool_consumer_instance_description: "Sample University (SomeLMS)",
+      user_id: "0ae836b9-7fc9-4060-006f-27b2066ac545",
+      custom_xstart: "2016-08-21T01:00:00Z",
+      custom_caliper_profile_url: "https://example.edu/lti/tc/cps",
+      custom_caliper_session_id: "1c519ff7-3dfa-4764-be48-d2fb35a2925a",
+      ext_com_somelms_example_course_section_instructor: "https://example.edu/faculty/1234"
+    };
+    
+    // Federated Session
+    var federatedSession = entityFactory().create(LtiSession, {
+      id: "https://example.edu/lti/sessions/b533eb02823f31024e6b7f53436c42fb99b31241",
+      user: actor,
+      messageParameters: messageParameters,
+      dateCreated: moment.utc("2018-11-15T10:15:00.000Z"),
+      startedAtTime: moment.utc("2018-11-15T10:15:00.000Z")
+    });
+    
     // Assert that key attributes are the same
     var event = eventFactory().create(ToolLaunchEvent, {
       id: uuid,
@@ -108,7 +137,8 @@ testUtils.readFile(path, function(err, fixture) {
       referrer: referrer,
       group: group,
       membership: membership,
-      session: session
+      session: session,
+      federatedSession: federatedSession
     });
     
     // Compare
